abc0bf6b157a313e39a63390bae6f45163edd6c2
najet83 <najet.benhamouda@cimf.tn>
2025-05-27 11:00:44 +0100

    MyContract

--- src/FinalProject.sol
+++ src/FinalProject.sol
@@ -1,5 +1,5 @@
-///SPDX-Licence-Identifier: MIT
-pragma solidity 0.8.30;
+//SPDX-Licence-Identifier: MIT
+pragma solidity 0.8.20;
 
 // import Openzeppelin Ownable contract
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -8,36 +8,36 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 // @author the author is Najet Ben Hamouda
 // @notice this contract is a simple wallet contract that allows users to deposit and withdraw ether
 // @dev this contract uses the OpenZeppelin Ownable contract to manage ownership
-contract FinalProjectContract is Ownable {
+contract FinalProject is Ownable {
 
-    // State variables
-    address public owner;
-
-   // call Ownable constructor of openzeppelin contract
-    constructor () Ownable(msg.sender){
-        owner = msg.sender;
-    }
-
-    
     // mapping of address to uint256
     // this mapping is used to store the balance of each address
     // the key is the address and the value is the balance
     mapping(address => uint256) public balanceOf;
+
+   // call Ownable constructor of openzeppelin contract
+    constructor () Ownable(msg.sender){
+      
+    }
     
-    
+    event FundsDeposited(address indexed sender, uint amount);
+    event FundsWithdrawn(address indexed receiver, uint amount);
+
     function deposit() external payable{
         require(msg.value > 0, "You must send some ether");
         balanceOf[msg.sender] += msg.value;
+        emit FundsDeposited(msg.sender,msg.value);
     }
 
     // make only the owner is the one who can call this function
-    function withdraw(uint256 calldata amount) external onlyOwner{
+    function withdraw(uint256 amount) external onlyOwner{
         require(amount > 0, "Amount must be greater than 0");
         require(balanceOf[msg.sender] >= amount, "Insufficient balance");
         balanceOf[msg.sender] -= amount;
         (bool sent, ) = msg.sender.call{value: amount}("");
         require(sent, "Failed to send Ether");
-        payable(owner).transfer(_amount);
+        emit FundsWithdrawn(msg.sender,amount);
+       
     }
 
     function getBalance() external view returns (uint256) {
---
+++ test/FinalProject.t.sol
@@ -0,0 +1,70 @@
+//SPDX-Licence-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "forge-std/Test.sol";
+import {FinalProject} from "../src/FinalProject.sol";
+
+contract FinalProjectTest is Test {
+
+FinalProject public finalProject;
+address public owner = makeAddr("owner");
+address public nonOwner = makeAddr("nonOwner");
+
+function setUp() public {
+    
+    vm.prank(owner);
+    finalProject = new FinalProject(); // Initialize the contract with an initial supply of 1000 ether
+    vm.deal(owner, 10 ether); // Give the owner 10 ether for testing
+    
+}
+
+//test case for the deposit function: the owner deposits 1 ether into the contract
+//the contract should update the owner's balance correctly: the balance is 1 ether
+function testDeposit() public {
+    vm.prank(owner); // Start impersonating the owner
+    finalProject.deposit{value: 1 ether}(); // owner deposits 1 ether
+    assertEq(finalProject.balanceOf(owner), 1 ether); // Check if the owner's balance is updated correctly
+    
+}
+
+//test case for the withdraw function: the owner deposits 1 ether into the contract and then he withdraws 0.5 ether from the contract
+//the contract should update the owner's balance correctly: the balance is 0.5 ether
+function testWithdrawAsOwner() public {
+    vm.startPrank(owner); // Start impersonating the owner
+    finalProject.deposit{value: 1 ether}(); // owner deposits 1 ether
+    finalProject.withdraw(0.5 ether); // owner withdraws 0.5 ether
+    assertEq(finalProject.balanceOf(owner), 0.5 ether); // Check if the owner's balance is updated correctly
+    vm.stopPrank(); // Stop impersonating the owner
+}
+//test case for the withdraw function: the owner deposits 1 ether into the contract and then he tries to withdraw more than his balance
+//the contract should revert with an error message: "Insufficient balance"
+
+
+function testRevertIfNotOwnerCallsWithdraw() public {
+    vm.prank(nonOwner);
+    vm.expectRevert();
+    finalProject.withdraw(1 ether);
+    
+}
+    
+function testRevertWithdrawMoreThanBalance() public {
+    vm.startPrank(owner); // Start impersonating the owner
+    finalProject.deposit{value: 1 ether}(); // owner deposits 1 ether
+    vm.expectRevert("Insufficient balance"); // Expect revert if owner tries to withdraw more than balance
+    finalProject.withdraw(2 ether); // owner tries to withdraw 2 ether
+    vm.stopPrank(); // Stop impersonating the owner
+}
+
+//test case for the GetBalance function: the owner deposits 1 ether into the contract
+//the contract should return the owner's balance correctly: the balance is 1 ether
+function testGetBalance() public {
+    vm.startPrank(owner); // Start impersonating the owner
+    finalProject.deposit{value: 1 ether}(); // owner deposits 1 ether
+    uint256 balance = finalProject.getBalance(); // Get the owner's balance
+    assertEq(balance, 1 ether); // Check if the balance is correct
+    vm.stopPrank(); // Stop impersonating the owner
+}
+
+
+
+}
